86985b3f9c25f31765b2bea3ded27d2968b52b4d
Joan Miralles <joan.miralles@gmail.com>
2018-11-17 14:09:16 +0100

    New TDD iteration

--- src/main/java/com/gameoflife/Cell.java
+++ src/main/java/com/gameoflife/Cell.java
@@ -1,7 +1,25 @@
 package com.gameoflife;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class Cell {
     private boolean alive;
+
+    static Map<Integer, Boolean> STATUS_BY_ALIVE_NEIGHBOURS = new HashMap<>();
+
+    static {
+        STATUS_BY_ALIVE_NEIGHBOURS.put(0, false);
+        STATUS_BY_ALIVE_NEIGHBOURS.put(1, false);
+        STATUS_BY_ALIVE_NEIGHBOURS.put(2, true);
+        STATUS_BY_ALIVE_NEIGHBOURS.put(3, true);
+        STATUS_BY_ALIVE_NEIGHBOURS.put(4, false);
+        STATUS_BY_ALIVE_NEIGHBOURS.put(5, false);
+        STATUS_BY_ALIVE_NEIGHBOURS.put(6, false);
+        STATUS_BY_ALIVE_NEIGHBOURS.put(7, false);
+        STATUS_BY_ALIVE_NEIGHBOURS.put(8, false);
+    }
+
 
     public Cell(boolean alive) {
         this.alive = alive;
@@ -11,7 +29,15 @@ class Cell {
         return alive;
     }
 
-    public void update() {
-        this.alive = false;
+    public void update(Neighbours neighbours) {
+        this.alive = STATUS_BY_ALIVE_NEIGHBOURS.get(neighbours.numberOfAlive());
+    }
+
+}
+
+class Neighbours {
+
+    public int numberOfAlive() {
+        return 0;
     }
 }
--- src/test/java/com/gameoflife/CellShould.java
+++ src/test/java/com/gameoflife/CellShould.java
@@ -1,11 +1,23 @@
 package com.gameoflife;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class CellShould {
+
+    private Neighbours neighbours;
+
+    @BeforeEach
+    void setUp() {
+        neighbours = mock(Neighbours.class);
+    }
 
     @Test
     void alive_when_start() {
@@ -13,11 +25,23 @@ public class CellShould {
         assertTrue(cell.isAlive());
     }
 
-    @Test
-    void die_in_contact_with_less_than_2_alive() {
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1})
+    void die_in_contact_with_less_than_2_alive(int numberOfAliveNeighbours) {
+        when(neighbours.numberOfAlive()).thenReturn(numberOfAliveNeighbours);
         Cell cell = new Cell(true);
-        cell.update();
+        cell.update(neighbours);
         assertFalse(cell.isAlive());
     }
+
+    @ParameterizedTest
+    @ValueSource(ints = {2, 3})
+    void survive_in_contact_with_2_or_3_alive(int numberOfAliveNeighbours) {
+        when(neighbours.numberOfAlive()).thenReturn(numberOfAliveNeighbours);
+        Cell cell = new Cell(true);
+        cell.update(neighbours);
+        assertTrue(cell.isAlive());
+    }
+
 }
 
