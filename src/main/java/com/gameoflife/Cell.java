307ed6354db13bfbeacafec16b78425107f7f7ca
Joan Miralles <joan.miralles@gmail.com>
2018-11-17 12:04:47 +0100

    Third TDD iteration

--- src/main/java/com/gameoflife/Cell.java
+++ src/main/java/com/gameoflife/Cell.java
@@ -2,16 +2,27 @@ package com.gameoflife;
 
 public class Cell {
 
-    public CellStatus liveStatus() {
-        return CellStatus.ALIVE;
+    private CellStatus liveStatus;
+
+    public Cell(CellStatus liveStatus) {
+        this.liveStatus = liveStatus;
+    }
+
+    public CellStatus getLiveStatus() {
+        return liveStatus;
     }
 
     public NeighboursCells getNeighbours() {
         return new NeighboursCells();
     }
 
+    public void update() {
+        this.liveStatus = CellStatus.DIED;
+    }
+
     public enum CellStatus {
-        ALIVE
+        ALIVE,
+        DIED
     }
 
     class NeighboursCells {
--- src/test/java/com/gameoflife/CellShould.java
+++ src/test/java/com/gameoflife/CellShould.java
@@ -3,8 +3,6 @@ package com.gameoflife;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.isA;
@@ -14,17 +12,22 @@ public class CellShould {
 
     @BeforeEach
     void setUp() {
+        cell = new Cell(Cell.CellStatus.ALIVE);
     }
 
     @Test
     void start_with_alive_status() {
-        cell = new Cell();
-        assertThat(cell.liveStatus(), is(Cell.CellStatus.ALIVE));
+        assertThat(cell.getLiveStatus(), is(Cell.CellStatus.ALIVE));
     }
 
     @Test
     void has_neighbours() {
-        cell = new Cell();
         assertThat(cell.getNeighbours(), isA(Cell.NeighboursCells.class));
+    }
+
+    @Test
+    void die_with_less_than_2_alive_neighbours() {
+        cell.update();
+        assertThat(cell.getLiveStatus(), is(Cell.CellStatus.DIED));
     }
 }
